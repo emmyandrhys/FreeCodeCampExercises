1bfb6893987ba33febc364f1df2e66a233c2ef3f
nstaley <emmyandrhys@gmail.com>
2021-05-17 10:51:39 -0500

    not rendering>

--- Timer/api.js
+++ Timer/api.js
@@ -1,194 +1,221 @@
 import * as React from "https://cdn.skypack.dev/react@17.0.1";
 import * as ReactDOM from "https://cdn.skypack.dev/react-dom@17.0.1";
-  
+//import * as React from "https://cdn.skypack.dev/react@17.0.1";
+//import * as ReactDOM from "https://cdn.skypack.dev/react-dom@17.0.1";
+
 var started = false;
 var paused = false;
 let countdown;
 
 class LengthControls extends React.Component {
-    render () {
-        return (
-            <div>
-                <h3 id={this.props.titleID}>{this.props.title}</h3>
-                <div>
-                    <button id={this.props.addID} className='adjustButton' data-type={this.props.dataType} onClick={this.handleTime(e)} value="-1">-1</button>
-                    <h2 id={this.props.lengthID}>{this.props.length}</h2>
-                    <button id={this.props.subID} className='adjustButton' onClick={this.handleTime(e)} value="1">+1</button>
-                </div>
-          </div>
-        )
-    }
+  render() {
+    return (
+      <main>
+        <h3 id={this.props.titleID}>{this.props.title}</h3>
+        <div>
+          <button
+            id={this.props.addID}
+            className="adjustButton"
+            data-type={this.props.dataType}
+            onClick={this.handleTime}
+            value="-1"
+          >
+            -1
+          </button>
+          <h2 id={this.props.lengthID}>{this.props.length}</h2>
+          <button
+            id={this.props.subID}
+            className="adjustButton"
+            onClick={this.handleTime}
+            value="1"
+          >
+            +1
+          </button>
+        </div>
+      </main>
+    );
+  }
 }
-  
 
+class TimerControls extends React.Component {
+  clock() {
+    var minutes = this.props.minutes;
+    var seconds = this.props.seconds;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+    return minutes + ":" + seconds;
+  }
+  render() {
+    return (
+      <div id="timer">
+        <h3 id="timer-label">{this.props.mode}</h3>
+        <h1 id="time-left">{this.clock()}</h1>
+        <audio
+          id="beep"
+          src="https://freesound.org/data/previews/3/3647_308-lq.mp3"
+        ></audio>
+      </div>
+    );
+  }
+}
 
 class Timer extends React.Component {
   constructor() {
     super();
-    this.state = { 
-                breakT: 5,
-                session: 25,
-                mode: 'session',
-                minutes: 25, 
-                seconds: 0,
-                }
-    this.handleTime=this.handleTime.bind(this)
-    this.reset=this.reset.bind(this)
+    this.state = {
+      breakT: 5,
+      session: 25,
+      mode: "session",
+      minutes: 25,
+      seconds: 0
+    };
+    this.handleTime = this.handleTime.bind(this);
+    this.reset = this.reset.bind(this);
     this.startTimer = this.startTimer.bind(this);
     this.stopTimer = this.stopTimer.bind(this);
-    this.tick=this.tick.bind(this)
-    this.switchMode=this.switchMode.bind(this)
+    this.tick = this.tick.bind(this);
+    this.switchMode = this.switchMode.bind(this);
   }
 
   startTimer() {
-    const {mode, minutes, seconds } = this.state;
-    document.getElementById('start_stop').textContent = 'Pause';
-    document.getElementById('reset').disabled = true;
-    document.querySelectorAll('.adjustButton').disabled = true;
+    const { mode, minutes, seconds } = this.state;
+    document.getElementById("start_stop").textContent = "Pause";
+    document.getElementById("reset").disabled = true;
+    document.querySelectorAll(".adjustButton").disabled = true;
     started = true;
+    let total;
     if (paused === true) {
-        const total = minutes * 60 + seconds;
-        paused = false;
-        countdown = setTimeout(tick(total), 1000)
+      total = minutes * 60 + seconds;
+      paused = false;
+      countdown = setTimeout(tick(total), 1000);
     } else {
-        const total = this.state[`${mode}`] * 60;
-        countdown = setTimeout(tick(total), 1000)
+      total = this.state[`${mode}`] * 60;
+      countdown = setTimeout(tick(total), 1000);
     }
   }
 
   stopTimer() {
     clearTimeout(countdown);
     paused = true;
-    document.getElementById('start_stop').textContent = 'Start';
-    document.getElementById('reset').disabled = false;
-    document.querySelectorAll('.adjustButton').disabled = false;
+    document.querySelectorAll(".adjustButton").disabled = false;
   }
 
   tick() {
-    const { minutes, seconds } = this.state;
-    if (seconds > 0 && minutes > 0 ) {
-        seconds--;
+    let minutes = this.state.minutes;
+    let seconds = this.state.seconds;
+    if (seconds > 0 && minutes > 0) {
+      seconds--;
     } else if (seconds === 0 && minutes > 0) {
-        seconds == 59;
-        minutes --;
-    } else { 
-        clearTimeout(this.tick)
-        this.switchMode()
+      seconds = 59;
+      minutes--;
+    } else {
+      clearTimeout(this.tick);
+      this.switchMode();
     }
     this.setState({
-        minutes : minutes,
-        seconds: seconds
-    })
-    setTimeout(this.tick(), 1000)
+      minutes: minutes,
+      seconds: seconds
+    });
+    setTimeout(this.tick(), 1000);
   }
-  
+
   switchMode() {
-    document.getElementById('beep').pause()
-    document.getElementById('beep').currentTime = 0;
+    document.getElementById("beep").pause();
+    document.getElementById("beep").currentTime = 0;
     let minutes;
     let mode;
     switch (this.state.mode) {
-        case 'session':
-            mode = 'break';
-            minutes = this.state.breakT;
-            break;
-        case 'break':
-            mode = 'session';
-            minutes = this.state.session;
-            break;
+      case "session":
+        mode = "break";
+        minutes = this.state.breakT;
+        break;
+      case "break":
+        mode = "session";
+        minutes = this.state.session;
+        break;
     }
-    document.getElementById('beep').play()
+    document.getElementById("beep").play();
     this.setState({
-        minutes: minutes,
-        seconds: 0,
-        mode: mode
-    })
-    setTimeout(this.tick(), 1000)
+      minutes: minutes,
+      seconds: 0,
+      mode: mode
+    });
+    setTimeout(this.tick(), 1000);
   }
 
   reset() {
     paused = false;
     started = false;
     this.setState({
-        breakT: 5,
-        session: 25,
-        mode: 'session',
-        minutes: 25,
-        seconds: 0
-    })
-    document.getElementById('beep').pause();
-    document.getElementById('beep').currentTime = 0;
-    document.getElementById('start_stop').textContent = 'Start';
-    document.querySelectorAll('.adjustButton').disabled = false; 
+      breakT: 5,
+      session: 25,
+      mode: "session",
+      minutes: 25,
+      seconds: 0
+    });
+  }
+  /*document.getElementById("beep").pause();
+    document.getElementById("beep").currentTime = 0;
+    document.getElementById("start_stop").textContent = "Start";
+    document.querySelectorAll(".adjustButton").disabled = false;*/
+
+  handleTime(e) {
+    let value = Number(e.target.value);
+    let type = e.target.dataType;
+    if (type === "session") {
+      let num = this.state.session;
+      num = num + value;
+      this.setState((session = num));
+    }
+    if (type === "break") {
+      let num = this.state.breakT;
+      num = num + value;
+      this.setState((breakT = num));
+    }
   }
 
-  handleTime(value) {
-    let value = Number(e.target.value)
-    if(this.state.mode === 'session') {
-        let num = this.state.session;
-        num = num + value;
-        this.setState(session=num);
-    }
-    if(this.state.mode === 'break') {
-        let num = this.state.breakT;
-        num = num + value;
-        this.setState(breakT=num);
-    }
-  }
-  
-
-  reset() {
-    this.setState(state => ({
-                  breakT: 5, 
-                  session: 20, 
-                  timer: {m:25,s:0}, 
-                  seconds: 0, 
-                  started: false
-                })
-      )
-    };
-  
   render() {
-    return(
-      <main id="main">
-        <div id="settings">
-        <LengthControls />
-          <div id="breakLength">
-            <h3 id="break-label">Break Length</h3>
-            <div>
-              <button id="break-decrement" className='adjustButton' onClick={this.handleTime('break', -1)}>-1</button>
-              <h2 id="break-length">{this.state.breakT}</h2>
-              <button id="break-increment" className='adjustButton' onClick={this.handleTime('break', 1)}>+1</button>
-            </div>
-          </div>
-          <LengthControls 
-            addID = "break-increment"
-            subId = "break-decrement"
-            title = "Break Length"
-            titleID = "break-label"
-            lengthID = "break-length"
-            length = {this.state.break}
-            onClick= {this.handleTime(e)}
-            dataType = 
-            />
-          
-        </div>
-      <div id="timer">
-        <h3 id="timer-label">Session</h3>
-        <h1 id="time-left">`${this.state.minutes}:${this.state.seconds}`</h1>
-        <audio id="beep" src="https://freesound.org/data/previews/3/3647_308-lq.mp3"></audio>
-      </div>
+    return (
+      <main>
+        <LengthControls
+          addID="session-increment"
+          subId="session-decrement"
+          title="Session Length"
+          titleID="session-label"
+          lengthID="session-length"
+          length={this.state.session}
+          onClick={this.handleTime}
+          dataType="session"
+        />
+        <LengthControls
+          addID="break-increment"
+          subId="break-decrement"
+          title="Break Length"
+          titleID="break-label"
+          lengthID="break-length"
+          length={this.state.breakT}
+          onClick={this.handleTime}
+          dataType="break"
+        />
+        <TimerControls
+          minutes={this.state.minutes}
+          seconds={this.state.seconds}
+          mode={this.state.mode}
+        />
         <div id="controls">
-            <button id="start_stop" onClick={started ? this.startTimer() : this.stopTimer()}></button>
-            <button id="reset" onClick={this.reset}></button>
+          <button
+            id="start_stop"
+            onClick={started ? this.startTimer() : this.stopTimer()}
+          >
+            {started ? "Pause" : "Start"}
+          </button>
+          <button id="reset" onClick={this.reset()}>
+            Reset
+          </button>
         </div>
-    </main>
-      
-      
-      
-     
+      </main>
     );
   }
 }
 
-ReactDOM.render(<Timer/>, document.getElementById('main'));
+ReactDOM.render(<Timer />, document.getElementById("main"));
